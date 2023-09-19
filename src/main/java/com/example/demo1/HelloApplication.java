61a088f024618703765dff0e1158390c09b251fd
Your Name <winchesterspn11@gmail.com>
2023-09-18 18:33:26 -0600

    nombre de lo cambiado

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -17,14 +17,14 @@ import java.io.IOException;
 import java.util.Optional;
 
 public class HelloApplication extends Application {
-    private Scene escena;
-    private BorderPane borderPane;
+    private Scene escena; //se intancia lo que es la pantalla
+    private BorderPane borderPane;   //es el que se colocan todos
     private MenuBar menuBar;
     private Image image=new Image("C:\\Users\\rodri\\OneDrive\\Escritorio\\kayakizaka46\\B0SuSSV.jpg");
     private Menu parcial_1,parcial_2,menuSalir;
     private MenuItem miCalculadora,tmiCalculadora,tloteria,salir;
-    private void crearUI(){
-        miCalculadora =new MenuItem("calculadora");
+    private void crearUI(){ //se crea la interfaz
+        miCalculadora =new MenuItem("calculadora"); //se crea el item
         tmiCalculadora =new MenuItem("cal_2");
         tloteria=new MenuItem("loteria");
         salir=new MenuItem("salir");
@@ -33,8 +33,8 @@ public class HelloApplication extends Application {
         menuSalir=new Menu("SALIR");
         tmiCalculadora.setOnAction((event)->new cal_2());
         tloteria.setOnAction((event -> new loteria()));
-        parcial_1=new Menu("pacial 1");
-        parcial_1.getItems().addAll(miCalculadora,tmiCalculadora,tloteria);
+        parcial_1=new Menu("pacial 1"); // se crea el menu
+        parcial_1.getItems().addAll(miCalculadora,tmiCalculadora,tloteria);// se agegan el menu
         salir.setOnAction(event -> salirm());
         menuSalir.getItems().addAll(salir);
         parcial_2=new Menu("parcial 2");
@@ -57,22 +57,24 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         crearUI();
-       borderPane =new BorderPane();
+       borderPane =new BorderPane();  //se crea el contenedor
        borderPane.setTop(menuBar);
-       escena=new Scene(borderPane,300,200);
+       escena=new Scene(borderPane,300,200); //se agrega el conetenedor a la ventana
+       escena.getStylesheets().add(getClass().getResource(
+               "/estilo1/estilos.css").toString()); // agrega la clase de estilos
 
 
 
         stage.setScene(escena);
         ImageView imageView=new ImageView();
-        imageView.setImage(image);
+        //imageView.setImage(image);
         imageView.setFitHeight(200);
         imageView.setFitWidth(300);
 
         borderPane.setCenter(imageView);
         stage.setTitle("mi primera calculadora ");
         stage.getIcons().add(new Image("C:\\Users\\rodri\\OneDrive\\Escritorio\\zakurazaka46\\QnAYD6QT_400x400.jpg"));
-       //stage.setMaximized(true)
+        stage.setMaximized(true);
          stage.show();
     }
 
--- src/main/java/com/example/demo1/vistas/loteria.java
+++ src/main/java/com/example/demo1/vistas/loteria.java
@@ -1,8 +1,13 @@
 package com.example.demo1.vistas;
 
+import java.util.*;
+
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -10,6 +15,8 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class loteria extends Stage {
     private Scene escena;
@@ -19,8 +26,21 @@ public class loteria extends Stage {
     private Image imazo,imagencarta,imagenDamwn;
     private GridPane gridPane;
     private BorderPane borderPane;
+    private int[][] arr1 = {
+            {0, 21, 19, 12},
+            {29, 25, 18, 3},
+            {13, 15, 5, 11},
+            {31, 33, 1, 23}
+    };
+    private int []arr2={34,16,11,18,12,25,21,7,10,28,15,20,24,29,19,5};
     private Button[][] arrbtn=new Button[4][4];
-    private  String[] arrImage={"1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","20_kousaka.jpg"};
+    private  int [] arrImage={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35};
+    private Timer timer;
+    private int tiempoCambioMazo = 2000;
+    private Set<Integer> imagenesUsadas = new HashSet<>();
+    private int totalImagenesUnicas;
+    private int indiceImagenActualDelMazo = -1;
+
     public loteria(){
         crearUI();
 
@@ -31,59 +51,184 @@ public class loteria extends Stage {
         this.show();
     }
 
-    private void crearUI(){
+    private void crearUI(){ //sse crea la interfaz
         CrearTablilla();
         crearMazo();
+
+        totalImagenesUnicas = arrImage.length;
         btnAnterior=new Button("<---");
-        btnAnterior.setPrefSize(200,100);
+
+        btnAnterior.setPrefSize(200,100);  //se le da el tamaño de los botones
+
         btnSiguiente=new Button("--->");
         btnSiguiente.setPrefSize(200,100);
-        botones=new HBox(btnAnterior,btnSiguiente);
-        carta=new VBox(gridPane,botones);
+
+        botones=new HBox(btnAnterior,btnSiguiente); //se coloca los botones en la hbox
+        carta=new VBox(gridPane,botones);// se agrega los botones y el gridpane en la cata que es un vbox
+        btnStart.setOnAction(event -> {
+            // Comenzar el temporizador para cambiar el mazo
+            iniciarTimer();
+        });
 
         hprincipal=new HBox(carta,mazo);
+        mazo.setTranslateX(250);
+        mazo.setSpacing(150);
         hprincipal.setPadding(new Insets(20));
         carta.setSpacing(20);
+
     }
 
     private void crearMazo() {
         imagenDamwn=new Image("C:\\Users\\rodri\\IdeaProjects\\demo1\\src\\main\\java\\imagen\\"+0+".png");
         ImageView imv=new ImageView(imagenDamwn);
-        imv.setFitHeight(100);
-        imv.setFitWidth(100);
-
+        imv.setFitHeight(300);
+        imv.setFitWidth(150);
         btnStart=new Button("START");
         btnStart.setPrefSize(200,100);
-
         mazo=new VBox(imv,btnStart);
     }
-
     private void CrearTablilla() {
+        gridPane = new GridPane();
+        AtomicInteger contador = new AtomicInteger();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
 
+                //Image imagencarta=new Image("C:\\Users\\rodri\\IdeaProjects\\demo1\\src\\main\\java\\imagen\\"+k+".png");
+                Image imagencarta = new Image("C:\\Users\\rodri\\IdeaProjects\\demo1\\src\\main\\java\\imagen\\" + arrImage[arr1[i][j]]+".png");
+                ImageView imv = new ImageView(imagencarta);
+                imv.setFitHeight(100);
+                imv.setFitWidth(100);
+                arrbtn[i][j] = new Button();
+                arrbtn[i][j].setGraphic(imv);
+                arrbtn[i][j].setPrefSize(100, 140);
+                gridPane.add(arrbtn[i][j], i, j);
+
+
+
+                int finalI = i;
+                int finalJ = j;
+                arrbtn[i][j].setOnAction(event -> {
+
+                    int valorBoton=arr1[finalI][finalJ];
+                    int valorMazo = obtenerValorMazoActual();
+
+
+                    if (valorBoton == valorMazo) {
+                        contador.getAndIncrement();
+                        arr1[finalI][finalJ]=0;
+                        Image imagencarta1 = new Image("C:\\Users\\rodri\\OneDrive\\Escritorio\\kayakizaka46\\G3bOYVQ.jpg");
+                        ImageView imv1 = new ImageView(imagencarta1);
+                        imv1.setFitHeight(100);
+                        imv1.setFitWidth(100);
+                        arrbtn[finalI][finalJ].setGraphic(imv1);
+                        System.out.println("¡Los valores son iguales! "+contador);
+                    } else if (imagenesUsadas.contains(valorBoton)){
+                        contador.getAndIncrement();
+                        arr1[finalI][finalJ]=0;
+                        Image imagencarta2 = new Image("C:\\Users\\rodri\\OneDrive\\Escritorio\\kayakizaka46\\G3bOYVQ.jpg");
+                        ImageView imv2 = new ImageView(imagencarta2);
+                        imv2.setFitHeight(100);
+                        imv2.setFitWidth(100);
+                        arrbtn[finalI][finalJ].setGraphic(imv2);
+                        System.out.println("¡la imagen ya habia salido tienes que estar mas al pendiente ! "+contador);
+
+                    }else{
+                        System.out.println("Los valores son diferentes.");
+                    }
+                    if(contador.get() ==16){
+
+                        GANASTE();
+                        detenerTimer();
+                    }
+
+
+                });
+
+            }
+        }
+    }
+    private void CrearTablilla2() {
         gridPane=new GridPane();
         int k=0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
 
-             //Image imagencarta=new Image("C:\\Users\\rodri\\IdeaProjects\\demo1\\src\\main\\java\\imagen\\"+k+".png");
-                Image imagencarta=new Image("C:\\Users\\rodri\\IdeaProjects\\demo1\\src\\main\\java\\imagen\\"+arrImage[k]);
+                //Image imagencarta=new Image("C:\\Users\\rodri\\IdeaProjects\\demo1\\src\\main\\java\\imagen\\"+k+".png");
+                Image imagencarta=new Image("C:\\Users\\rodri\\IdeaProjects\\demo1\\src\\main\\java\\imagen\\"+arrImage[arr2[k]]);
                 ImageView imv=new ImageView(imagencarta);
                 imv.setFitHeight(100);
                 imv.setFitWidth(100);
-
-
                 arrbtn[i][j]=new Button();
                 arrbtn[i][j].setGraphic(imv);
                 arrbtn[i][j].setPrefSize(100,140);
                 gridPane.add(arrbtn[i][j],i,j);
-                if (k == 9) {
-                    k=0;
-
-                }else{
-                    k++;
-                }
+                k++;
             }
 
         }
     }
+    private void iniciarTimer() {
+        int n=0;
+        if (timer != null) {
+            timer.cancel();
+        }
+
+        timer = new Timer();
+        TimerTask tareaCambioMazo = new TimerTask() {
+
+            @Override
+            public void run() {
+
+                // Cambiar la imagen del mazo aquí
+
+                Platform.runLater(() -> {
+                    if (imagenesUsadas.size() < totalImagenesUnicas) {
+                        cambiarImagenMazo();
+
+                    } else {
+                        detenerTimer();
+                    }
+
+                });
+            }
+        };
+
+        timer.scheduleAtFixedRate(tareaCambioMazo, tiempoCambioMazo, tiempoCambioMazo);
+    }
+
+    private void cambiarImagenMazo() {
+        // Aquí cambia la imagen del mazo sin repetirlas
+        Random random = new Random();
+        int indiceImagen;
+        // Busca una imagen aleatoria que aún no se haya utilizado
+        do {
+            indiceImagen = random.nextInt(arrImage.length);
+        } while (imagenesUsadas.contains(indiceImagen));
+        imagenesUsadas.add(indiceImagen);
+        indiceImagenActualDelMazo = indiceImagen;
+        Image nuevaImagen = new Image("C:\\Users\\rodri\\IdeaProjects\\demo1\\src\\main\\java\\imagen\\" + arrImage[indiceImagen] + ".png");
+        ImageView imv = new ImageView(nuevaImagen);
+        imv.setFitHeight(300);
+        imv.setFitWidth(150);
+        mazo.getChildren().set(0, imv); // Reemplaza la imagen del mazo con la nueva imagen
+    }
+    private void detenerTimer() {
+        if (timer != null) {
+            timer.cancel();
+        }
+
+    }
+    private int obtenerValorMazoActual() {
+        return indiceImagenActualDelMazo;
+    }
+    private void GANASTE() {
+        Alert alert=new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("FELICIDADES HAS GANADO ERES UN CRACK");
+        Optional<ButtonType> option=alert.showAndWait();
+        if(option.get() == ButtonType.OK){
+            this.close();
+        }
+
+    }
 }
+
